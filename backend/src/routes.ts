ce7b897350f7a844e0025304bf3d58923c3167de
Joao Souza <joaoosouza07@gmail.com>
2023-04-15 22:09:55 -0300

    :sparkles: Add and Remove order items

--- backend/src/routes.ts
+++ backend/src/routes.ts
@@ -14,6 +14,8 @@ import { ListByCategoryController } from "./controllers/product/ListByCategoryCo
 
 import { CreateOrderController } from "./controllers/order/CreateOrderController";
 import { RemoveOrderController } from "./controllers/order/RemoveOrderController";
+import { AddItemController } from "./controllers/order/AddItemController";
+import { RemoveItemController } from "./controllers/order/RemoveItemController";
 
 // MIDDLEWARE DE AUTENTICAÇÃO
 import isAuthenticated from "./middlewares/isAuthenticated";
@@ -46,5 +48,9 @@ router.get("/category/product", isAuthenticated, new ListByCategoryController().
 router.post("/order", isAuthenticated, new CreateOrderController().handle);
 
 router.delete("/order", isAuthenticated, new RemoveOrderController().handle);
+
+router.post("/order/add", isAuthenticated, new AddItemController().handle);
+
+router.delete("/order/remove", isAuthenticated, new RemoveItemController().handle);
 
 export { router };

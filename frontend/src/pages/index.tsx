aba2ac0d3006620b58b255fa0b761b80895efb5c
Joao Souza <joaoosouza07@gmail.com>
2023-04-16 21:42:20 -0300

    :lipstick: Reset CSS

---
+++ frontend/src/pages/index.tsx
@@ -0,0 +1,15 @@
+import Head from "next/head";
+
+export default function Home() {
+  return(
+    <>
+      <Head>
+        <title>FastBurguer</title>
+      </Head>
+
+      <div>
+        <h1>Fast Burguer</h1>
+      </div>
+    </>
+  );
+}
